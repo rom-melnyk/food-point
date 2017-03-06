450e2f06c7c220fdcdcc5e38bc179c44be628ca3
Roman <email.rom.melnyk@gmail.com>
2017-03-06 21:59:19 +0100

    CSS: Site menu

--- index.php
+++ index.php
@@ -6,51 +6,61 @@
 </head>
 <body onload="FP.run();">
     <header>
-        <span class="logo"></span>
+        <div class="top-level-wrapper">
+            <span class="logo"></span>
+        </div>
     </header>
 
     <nav class="site-menu">
-        <ul>
-            <li>Страви</li>
-            <li>Замовити</li>
-            <li>Цікаве</li>
-            <li>Про нас</li>
-        </ul>
+        <div class="top-level-wrapper">
+            <ul>
+                <li><a href="#">Страви</a></li>
+                <li><a href="#">Замовити</a></li>
+                <li><a href="#">Цікаве</a></li>
+                <li><a href="#">Про нас</a></li>
+            </ul>
+        </div>
     </nav>
 
     <section class="dishes">
-        <h1>Що є поїсти?</h1>
-        Here be menu
+        <div class="top-level-wrapper">
+            <h1>Що є поїсти?</h1>
+            Here be menu
+        </div>
     </section>
 
     <section class="order">
-        <h1>Замовити їжу просто!</h1>
-        The order info here
+        <div class="top-level-wrapper">
+            <h1>Замовити їжу просто!</h1>
+            The order info here
+        </div>
     </section>
 
     <footer class="row">
-        <div class="col-1">
-            &laquo;Фуд Поінт&raquo;<br/>
-            вул. Валова 5а<br/>
-            Івано-Франківськ<br/>
-            <span class="phone-num">(067)11-67-130</span>
-        </div>
-        <div class="col-1">
-            Інші контакти для скарг та пропозицій з 10:00 до 19:00<br/>
-            <span class="phone-num">(067) 11-67-130</span>
-        </div>
-        <div class="col-1">
-            <ul>
-                <li>Страви</li>
-                <li>Замовити</li>
-                <li>Цікаве</li>
-                <li>Про нас</li>
-            </ul>
-        </div>
-        <div class="col-1">
-            <span class="logo"></span>
-            &copy; Food Point 2014&ndash;2017<br/>
-            Вдосконалюємось кожного дня!
+        <div class="top-level-wrapper">
+            <div class="col-1">
+                &laquo;Фуд Поінт&raquo;<br/>
+                вул. Валова 5а<br/>
+                Івано-Франківськ<br/>
+                <span class="phone-num">(067)11-67-130</span>
+            </div>
+            <div class="col-1">
+                Інші контакти для скарг та пропозицій з 10:00 до 19:00<br/>
+                <span class="phone-num">(067) 11-67-130</span>
+            </div>
+            <div class="col-1">
+                <ul>
+                    <li>Страви</li>
+                    <li>Замовити</li>
+                    <li>Цікаве</li>
+                    <li>Про нас</li>
+                </ul>
+            </div>
+            <div class="col-1">
+                <span class="logo"></span>
+                &copy; Food Point 2014&ndash;2017<br/>
+                Вдосконалюємось кожного дня!
+            </div>
         </div>
     </footer>
 </body>
