908a65b4eb7dc67b58ab8c871a21692c74a23c82
Roman <email.rom.melnyk@gmail.com>
2017-05-28 00:04:48 +0200

    Header

--- server/php/includes/header.php
+++ server/php/includes/header.php
@@ -1,11 +1,14 @@
 <header class="row">
-    <div class="column-3 logo">
-        <a href="/">
-            <img class="logo-big" alt="FoodPoint" src="/gfx/logo.png">
-        </a>
+    <div class="header-logo">
+        <img class="logo" alt="FoodPoint" src="/gfx/logo.png">
         <div class="slogan">Ми готуємо з любов'ю</div>
     </div>
-    <div class="column-1 contact-us">
-        Some text
-    </div>
+    <nav class="site-menu">
+        <ul>
+            <li class="menu-item">Головна</li>
+            <li class="menu-item">Поїсти</li>
+            <li class="menu-item">Що нового?</li>
+            <li class="menu-item">Про нас</li>
+        </ul>
+    </nav>
 </header>
