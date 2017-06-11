0ba8324ce47c094e60807dded3b5c653faa2b1be
Roman <email.rom.melnyk@gmail.com>
2017-06-11 18:25:00 +0200

    Main page beutifying

--- server/php/includes/dishes.php
+++ server/php/includes/dishes.php
@@ -3,17 +3,42 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/php/data-models/image-model.php';
 
 
 function get_group_header($group) {
-    $img = $group['image']
-        ? '<img class="image" src="' . model\HTML_TARGET_DIR . $group['image'] . '" />'
-        : '';
+    $img = '';
+    if ($group['image']) {
+        if ($group['name'] || $group['description']) {
+            $img = '<div class="column-2">'
+                . '<div class="image-wrapper">'
+                . '<span class="image" style="background-image: url(\''
+                    . model\HTML_TARGET_DIR . $group['image']
+                    . '\');"></span>'
+                . '</div>'
+                . '</div>';
+        } else {
+            $img = '<div class="column-12">'
+                . '<img class="image" src="' . model\HTML_TARGET_DIR . $group['image'] . '" />'
+                . '</div>';
+        }
+    }
+
+    $text_cell_classname = 'column-' . ($group['image'] ? 10 : 12) . ' all-text';
     $name = $group['name']
         ? '<div class="name">' . $group['name'] . '</div>'
         : '';
     $description = $group['description']
         ? '<div class="description">' . $group['description'] . '</div>'
         : '';
+    $all_text = $name || $description
+        ? (
+            '<div class="' . $text_cell_classname . '">'
+            . $name . $description
+            . '</div>'
+        )
+        : '';
 
-    return '<div class="row group-data">' . $img . $name . $description . '</div>';
+    return '<div class="row group-data">'
+        . $img
+        . $all_text
+        . '</div>';
 }
 
 
@@ -40,22 +65,45 @@ function get_group($group) {
 
 function get_dish($dish) {
     $img = $dish['image']
-        ? '<img class="image" src="' . model\HTML_TARGET_DIR . $dish['image'] . '" />'
+        ? (
+            '<div class="column-2">'
+            . '<div class="image-wrapper">'
+            . '<span class="image" src="background-image: url(\''
+                . model\HTML_TARGET_DIR . $dish['image']
+                . '\')"></span>'
+            . '</div>'
+            . '</div>'
+        )
         : '';
+
+
     $name = $dish['name']
-        ? '<div class="name">' . $dish['name'] . '</div>'
+        ? '<span class="name">' . $dish['name'] . '</span>'
+        : '';
+    $size = $dish['size']
+        ? '<span class="size">' . $dish['size'] . '</span>'
         : '';
     $description = $dish['description']
         ? '<div class="description">' . $dish['description'] . '</div>'
         : '';
-    $size = $dish['size']
-        ? '<div class="size">' . $dish['size'] . '</div>'
-        : '';
+    $all_text = '<div class="column-' . ($dish['image'] ? 8 : 10) . ' all-text">'
+        . $name . $size
+        . $description
+        . '</div>';
+
     $price = $dish['price']
-        ? '<div class="price">' . $dish['price'] . '</div>'
+        ? (
+            '<div class="column-2">'
+            . '<div class="price">' . $dish['price'] . '</div>'
+            . '</div>'
+        )
         : '';
 
-    return '<div class="row group-data">' . $img . $name . $size . $description . $price . '</div>';
+    return '<div class="row dish">'
+        . $img
+        . $all_text
+        . $price
+        . '</div>';
 }
 
 ?>
